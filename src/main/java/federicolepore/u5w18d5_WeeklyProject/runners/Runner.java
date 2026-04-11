b5aefd66e21cda29e61e9607fcd51bee1e0a49dc
Dedes <federico.lepore73@gmail.com>
2026-04-11 02:10:33 +0200

    istanze e modifica create in application.properties

--- src/main/java/federicolepore/u5w18d5_WeeklyProject/runners/Runner.java
+++ src/main/java/federicolepore/u5w18d5_WeeklyProject/runners/Runner.java
@@ -2,6 +2,9 @@ package federicolepore.u5w18d5_WeeklyProject.runners;
 
 import com.github.javafaker.Faker;
 import federicolepore.u5w18d5_WeeklyProject.entities.Edificio;
+import federicolepore.u5w18d5_WeeklyProject.entities.Postazione;
+import federicolepore.u5w18d5_WeeklyProject.entities.Utente;
+import federicolepore.u5w18d5_WeeklyProject.enumerators.TipoPostazione;
 import federicolepore.u5w18d5_WeeklyProject.services.EdificioService;
 import federicolepore.u5w18d5_WeeklyProject.services.PostazioneService;
 import federicolepore.u5w18d5_WeeklyProject.services.PrenotazioneService;
@@ -10,6 +13,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @Slf4j
@@ -36,16 +42,56 @@ public class Runner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        // creo gli edifici
+        // creo gli edifici e li aggiungo in una lista
+        List<Edificio> edifici = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
-            Edificio edificio = new Edificio(
+            Edificio e = new Edificio(
                     faker.company().name(),
                     faker.address().streetAddress(),
                     faker.address().city()
             );
-            edificioService.save(edificio);
+            edifici.add(edificioService.save(e));
+            System.out.println(e);
         }
 
+        // adesso ciclando la lista degli edifici per ognuno di questi creo tre postazioni, ua per tipo
+        for (int i = 0; i < edifici.size(); i++) {
+            Postazione p1 = new Postazione(
+                    "Postazione di lavoro privata per lavorare senza disturbi",
+                    TipoPostazione.PRIVATO,
+                    1,
+                    edifici.get(i)
+            );
+            Postazione p2 = new Postazione(
+                    "Postazione di lavoro in ambiente condiviso per condividere idee e ispirazione",
+                    TipoPostazione.OPEN_SPACE,
+                    10,
+                    edifici.get(i)
+            );
+            Postazione p3 = new Postazione(
+                    "Sala riunioni super attrezzata per connetterti con colleghi e clienti da tutto il mondo come se fossero insieme a te ",
+                    TipoPostazione.SALA_RIUNIONI,
+                    25,
+                    edifici.get(i)
+            );
+            postazioneService.save(p1);
+            postazioneService.save(p2);
+            postazioneService.save(p3);
+            System.out.println(p1);
+            System.out.println(p2);
+            System.out.println(p3);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            Utente u = new Utente(
+                    faker.name().username(),
+                    faker.name().firstName(),
+                    faker.name().lastName(),
+                    faker.internet().emailAddress()
+            );
+            utenteService.save(u);
+            System.out.println(u);
+        }
 
     }
 }
