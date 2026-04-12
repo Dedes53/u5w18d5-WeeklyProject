0aa69eba220095a1758806801492655503a45c76
Dedes <federico.lepore73@gmail.com>
2026-04-12 16:02:46 +0200

    Create eccezioni

---
+++ src/main/java/federicolepore/u5w18d5_WeeklyProject/exceptions/PostazionePienaException.java
@@ -0,0 +1,7 @@
+package federicolepore.u5w18d5_WeeklyProject.exceptions;
+
+public class PostazionePienaException extends RuntimeException {
+    public PostazionePienaException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/federicolepore/u5w18d5_WeeklyProject/exceptions/PrenotazioneDuplicataException.java
@@ -0,0 +1,7 @@
+package federicolepore.u5w18d5_WeeklyProject.exceptions;
+
+public class PrenotazioneDuplicataException extends RuntimeException {
+    public PrenotazioneDuplicataException(String message) {
+        super(message);
+    }
+}
